71a55973b90ac46c128cb22d109a458319f2373f
Yeo Wei Hng <69447385+YEOWEIHNGWHYELAB@users.noreply.github.com>
2021-09-14 16:43:08 +0800

    Add files via upload

---
+++ Week 3/exe4/src/com/whyelab/Main.java
@@ -0,0 +1,12 @@
+package com.whyelab;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+
+        System.out.println(c.getArea());
+        c = new Circle(1, 2, 5);
+        System.out.println(c.getArea());
+
+    }
+}
